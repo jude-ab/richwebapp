ee859d5013264f29ac1bf6a9a47d143ace37ca50
jude abouhajar <judeabouhajar@judes-air.home>
2023-11-30 00:44:23 +0000

    search functionality

--- week11/notesapp/src/components/notesList.js
+++ week11/notesapp/src/components/notesList.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Notes from "./Notes";
 
 function NotesList({ notes, deleteNote, updateNote }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredNotes = notes.filter((note) =>
+    note.text.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="notes-container">
-      {notes.map((note) => (
+      <input
+        type="text"
+        placeholder="Search notes..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {filteredNotes.map((note) => (
         <Notes
           key={note.id}
           note={note}
