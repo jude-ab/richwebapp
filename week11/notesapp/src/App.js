36b56e57a2ab4d1587dcf1dac120b17d06c30c8d
jude abouhajar <judeabouhajar@judes-air.home>
2023-11-30 01:38:55 +0000

    theme

--- week11/notesapp/src/App.js
+++ week11/notesapp/src/App.js
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+// App.js
+import React, { useState, useEffect } from "react";
 import CreateNote from "./components/createNote";
 import NotesList from "./components/notesList";
 import "./App.css";
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [theme, setTheme] = useState("light"); // Theme state
+
+  useEffect(() => {
+    // Update the class on the body tag
+    document.body.className = theme;
+  }, [theme]); // This effect runs when the theme state changes
 
   const addNote = (newNote) => {
     setNotes([...notes, { ...newNote, id: Date.now() }]);
@@ -21,8 +28,22 @@ function App() {
   };
 
   return (
-    <div className="container">
+    <div className={`app ${theme}`}>
+      {" "}
+      {/* Theme class applied here */}
       <h2>Notes App</h2>
+      <div className="theme-selector">
+        <label htmlFor="theme">Choose a theme:</label>
+        <select
+          id="theme"
+          value={theme}
+          onChange={(e) => setTheme(e.target.value)}
+        >
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+          {/* Add more themes as needed */}
+        </select>
+      </div>
       <CreateNote addNote={addNote} />
       <NotesList
         notes={notes}
